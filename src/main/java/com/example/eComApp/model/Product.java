ee64f95969d12971d3505b75ccb14344de7218d1
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-03-10 16:29:16 +0530

    update Product Model class

--- src/main/java/com/example/eComApp/model/Product.java
+++ src/main/java/com/example/eComApp/model/Product.java
@@ -4,17 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.http.HttpStatusCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Product {
     @Id
-    private int prodId;
-    private String prodName;
-    private int price;
-    public Product(){
-
-    }
+    private int id;
+    private String name;
+    private String desc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
 }
